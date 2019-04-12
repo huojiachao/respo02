c0e313d4890cdfa387b8ef92645c91c532e19b50
hjc <863065710@qq.com>
2019-04-13 00:30:10 +0800

    xiugai

--- src/com/itcast/day02/Test01.java
+++ src/com/itcast/day02/Test01.java
@@ -20,8 +20,10 @@ public class Test01 {
         System.out.println("我是你爸爸");
     }
     public void Test02(){
+        //zhushi
         int i = 4;
         int n = 5;
         System.out.println(i + n);
+        System.out.println(i * n);
     }
 }
