aea6ccc666bb6ed511a3bda3ccc05ea4a15498b2
hjc <863065710@qq.com>
2019-04-13 00:14:12 +0800

    qqq

---
+++ src/com/itcast/day02/Test01.java
@@ -0,0 +1,22 @@
+package com.itcast.day02;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+        System.out.println("我是你爸爸");
+    }
+}
